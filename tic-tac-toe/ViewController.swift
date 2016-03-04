0c31e088345ed114249b6612389f06d614f3ab8c
vladimirkush <vladimirk@gmail.com>
2016-03-04 11:31:06 +0300

    added cell index print added X-O change logic

--- tic-tac-toe/ViewController.swift
+++ tic-tac-toe/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    var currentPlayer = "X"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,14 +23,30 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 4
+        return 3
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 3
     }
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        return collectionView.dequeueReusableCellWithReuseIdentifier("CellID", forIndexPath: indexPath)
+        
+         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CellID", forIndexPath: indexPath) as! CollectionViewCell
+        cell.lblCelltext.text = "\(indexPath.row), \(indexPath.section)"
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        print("Row \(indexPath.row), section: \(indexPath.section)")
+        let cell = collectionView.cellForItemAtIndexPath(indexPath) as! CollectionViewCell
+        if currentPlayer == "X"{
+            cell.lblCelltext.text="X"
+            currentPlayer="O"
+        }else if currentPlayer == "O"{
+            cell.lblCelltext.text="O"
+            currentPlayer="X"
+        }
+        //NSiindexpath is an index object
     }
     
     
